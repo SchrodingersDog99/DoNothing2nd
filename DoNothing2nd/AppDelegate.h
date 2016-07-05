f2c4c31d79ad6efad31cbc5d5306063aca5846c3
SchrodingersDog99 <joneeyli@qq.com>
2016-07-05 14:41:44 -0400

    Initial Commit

---
+++ DoNothing2nd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoNothing2nd
+//
+//  Created by DonghuiLi on 16/7/5.
+//  Copyright © 2016年 SchrodingersDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoNothing2nd/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DoNothing2nd
+//
+//  Created by DonghuiLi on 16/7/5.
+//  Copyright © 2016年 SchrodingersDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
